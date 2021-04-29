a5cc0b91faae3e81d6f0d9ab81a02014e28f319d
hanishkvc <hanishkvc@gmail.com>
2021-04-29 11:47:14 +0530

    A direct javascript hello world

---
+++ Firebase.Admin/create_firestore.js
@@ -0,0 +1,16 @@
+
+
+lStates = [
+    'KA',
+    'KL',
+    'TN',
+    ]
+
+console.log('Hello world')
+
+lStates.forEach((tState) => {
+    console.log(tState)
+})
+
+
+/* vim: set ts=4 sts=4 expandtab :*/
