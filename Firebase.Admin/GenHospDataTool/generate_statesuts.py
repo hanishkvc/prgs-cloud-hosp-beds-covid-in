348f75304d49b3d3bb31d9f65ecb44e4beec80f2
hanishkvc <hanishkvc@gmail.com>
2021-04-30 02:23:27 +0530

    Scripts: Pick State/UT code from a csv

--- Firebase.Admin/GenHospDataTool/generate_statesuts.py
+++ Firebase.Admin/GenHospDataTool/generate_statesuts.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 
+
 import pandas
+
+
+p = pandas.read_csv("./statesuts.code.csv")
+dStates = {}
+for i in range(37):
+	dStates[p.iloc[i,1].strip()] = p.iloc[i,0]
+print(dStates)
 
 p=pandas.read_csv("hospital_directory.csv")
 f = open("/tmp/t.1","w+")
 for s in p.State.unique():
-	print("{}: ".format(s)+"{", file=f)
+	print("{}: ".format(dStates[s])+"{", file=f)
 	print("\t'Name': '{}',".format(s), file=f)
 	i = 0
 	for d in p[p.State == s].District.unique():
