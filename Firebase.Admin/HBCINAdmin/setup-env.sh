93a4881d35e308a76fd0cafc8dd170b1d7f3da58
hanishkvc <hanishkvc@gmail.com>
2021-04-29 17:05:45 +0530

    FAdmin: Setup env variables for Server and Local emulator
    Also update .gitignore wrt node_modules

---
+++ Firebase.Admin/HBCINAdmin/setup-env.sh
@@ -0,0 +1,14 @@
+
+function server_auth() {
+	echo "INFO: Setting up Server Auth"
+	export GOOGLE_APPLICATION_CREDENTIALS="/home/hkvctest/hkvc/Hosp-Beds-Covid-IN/SECURE/fb-service-account.json"
+}
+
+function emulator() {
+	echo "ALERT: Targetting emulator"
+	export FIRESTORE_EMULATOR_HOST="localhost:8080"
+}
+
+server_auth
+emulator
+
