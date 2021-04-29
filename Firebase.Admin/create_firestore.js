c41615dbb150b73359af54c67fc5a078a527adcc
hanishkvc <hanishkvc@gmail.com>
2021-04-29 12:23:44 +0530

    FAdmin: Create a function for states

--- Firebase.Admin/create_firestore.js
+++ Firebase.Admin/create_firestore.js
@@ -1,4 +1,8 @@
-
+/*
+ * Create the Data schema in Firestore
+ * HanishKVC, 2021
+ * GPL
+ */
 
 let lStates = {
     'KA': {
@@ -21,17 +25,21 @@ let lStates = {
 console.log('Hello world')
 console.log(lStates['KA'])
 
-for(tStateKey in lStates) {
-    tName = null
-    tState = lStates[tStateKey]
-    for(tKey in tState) {
-        if (tKey === 'Name') {
-            tName = tState[tKey]
-        } else {
-            console.log(tStateKey, tName, tKey, tState[tKey])
+function create_states() {
+    for(tStateKey in lStates) {
+        tName = null
+        tState = lStates[tStateKey]
+        for(tKey in tState) {
+            if (tKey === 'Name') {
+                tName = tState[tKey]
+            } else {
+                console.log(tStateKey, tName, tKey, tState[tKey])
+            }
         }
     }
 }
 
+create_states()
 
-/* vim: set ts=4 sts=4 expandtab :*/
+
+/* vim: set ts=4 sts=4 sw=4 expandtab :*/
