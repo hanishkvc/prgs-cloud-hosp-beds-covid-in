18a775d34c8e75585b39abdb88ca708f00c3b93f
hanishkvc <hanishkvc@gmail.com>
2021-04-30 02:41:15 +0530

    Scripts: Generate StatesUTs cleanup
    Add some missing or old/new name based State codes
    
    Skip the invalid state entry in Hospitals data

--- Firebase.Admin/GenHospDataTool/generate_statesuts.py
+++ Firebase.Admin/GenHospDataTool/generate_statesuts.py
@@ -7,17 +7,23 @@ import pandas
 p = pandas.read_csv("./statesuts.code.csv")
 dStates = {}
 for i in range(37):
-	dStates[p.iloc[i,1].strip()] = p.iloc[i,0]
+	dStates[p.iloc[i,0].strip()] = p.iloc[i,1].strip()
 print(dStates)
+
+dStates['Odisha'] = dStates['Orissa']
+dStates['Puducherry'] = dStates['Pondicherry']
+dStates['Telangana'] = 'TS'
 
 p=pandas.read_csv("hospital_directory.csv")
 f = open("/tmp/t.1","w+")
 for s in p.State.unique():
-	print("{}: ".format(dStates[s])+"{", file=f)
-	print("\t'Name': '{}',".format(s), file=f)
+	if s.startswith('North Twenty'):
+		continue
+	print("    '{}': ".format(dStates[s])+"{", file=f)
+	print("{:8}'Name': '{}',".format(" ",s), file=f)
 	i = 0
 	for d in p[p.State == s].District.unique():
 		i += 1
-		print("\t'DId{:02}': '{}',".format(i,d), file=f)
-	print("\t}", file=f)
+		print("{:8}'DId{:02}': '{}',".format(" ",i,d), file=f)
+	print("{:8}".format(' ')+"},", file=f)
 f.close()
