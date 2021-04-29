01a14aaa275870a7d0242147fcb18c0a94c3c0f1
hanishkvc <hanishkvc@gmail.com>
2021-04-29 12:01:47 +0530

    Test out json in place of a array

--- Firebase.Admin/create_firestore.js
+++ Firebase.Admin/create_firestore.js
@@ -1,16 +1,26 @@
 
 
-lStates = [
-    'KA',
-    'KL',
-    'TN',
-    ]
+let lStates = {
+    'KA': [
+        'Tumakuru',
+        'Bengaluru',
+        ],
+    'KL': [
+        'Ernakulam',
+        'Thiruvananthapuram',
+        ],
+    'TN': [
+        'Chennai',
+        'Vellore',
+        ]
+    }
 
 console.log('Hello world')
+console.log(lStates['KA'])
 
-lStates.forEach((tState) => {
-    console.log(tState)
-})
+for(tState in lStates) {
+    console.log(tState, lStates[tState])
+}
 
 
 /* vim: set ts=4 sts=4 expandtab :*/
