1ac1ffd7dd56ec4e2992516c6fae4b425a73627e
hanishkvc <hanishkvc@gmail.com>
2021-05-04 04:30:32 +0530

    FAdmin:DBX: Fix some oversights, More informative log
    Add the missing require wrt hlpr module.
    
    Use cName variable and not a string containing cName wrt
    collection name.
    
    Make _import_collection available both internally as well as
    externally.
    
    Now the collection which is being imported is also mentioned in
    the generated logs.

--- Firebase.Admin/HBCINAdmin/dbx.js
+++ Firebase.Admin/HBCINAdmin/dbx.js
@@ -5,22 +5,29 @@
  */
 
 
+var hlpr = require('./hlpr');
+
+
 /**
  * Import a java object as a firestore collection of documents
- * It can be used to create a new collection
- * or to update documents in a existing collection
- * or to add new documents to a existing collection
+ * It can be used to create a new collection of documents.
+ * or to update some or all documents in a existing collection
+ *     Even if updating only few fields in the document
+ *     data for all fields needs to be provided.
+ *     Else missing fields will be removed from the updated doc.
+ * or to add new documents to a existing collection.
  */
-exports._import_collection = function (db, cName, oCollection) {
-    curDC=db.collection("cName");
+function _import_collection(db, cName, oCollection) {
+    curDC=db.collection(cName);
     for(tDocKey in oCollection) {
         tDoc = oCollection[tDocKey]
         curDC.doc(tDocKey)
             .set(tDoc)
-            .then(hlpr.msg_success.bind(null, tDocKey, "ImportCollection:Adding"))
-            .catch(hlpr.msg_failure.bind(null, tDocKey, "ImportCollection:Adding"))
+            .then(hlpr.msg_success.bind(null, tDocKey, `ImportCollection:${cName}:Adding`))
+            .catch(hlpr.msg_failure.bind(null, tDocKey, `ImportCollection:${cName}:Adding`))
     }
 }
+exports._import_collection = _import_collection
 
 
 exports.import_collection = function (db, cName, cFile=null) {
