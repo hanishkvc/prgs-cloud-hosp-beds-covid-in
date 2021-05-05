826e2374583635d6cc49d82082456458f86683b6
hanishkvc <hanishkvc@gmail.com>
2021-05-05 18:58:53 +0530

    Scripts: Organise generate regions helper script

--- Firebase.Admin/GenHospDataTool/generate_statesuts.py
+++ Firebase.Admin/GenHospDataTool/generate_statesuts.py
@@ -6,36 +6,48 @@
 import pandas
 
 
-p = pandas.read_csv("./statesuts.code.csv")
-dStates = {}
-for i in range(p.shape[0]):
-	dStates[p.iloc[i,0].strip()] = p.iloc[i,1].strip()
-print(dStates)
+def get_statecodes(fName):
+    p = pandas.read_csv(fName)
+    dStates = {}
+    for i in range(p.shape[0]):
+	    dStates[p.iloc[i,0].strip()] = p.iloc[i,1].strip()
+    print(dStates)
 
-dStates['Odisha'] = dStates['Orissa']
-dStates['Puducherry'] = dStates['Pondicherry']
-dStates['Telangana'] = 'TS'
+    dStates['Odisha'] = dStates['Orissa']
+    dStates['Puducherry'] = dStates['Pondicherry']
+    dStates['Telangana'] = 'TS'
+    return dStates
 
-p=pandas.read_csv("hospital_directory.csv")
-f = open("/tmp/t.1","w+")
-print('{', file=f)
-for s in p.State.unique():
-	if s.startswith('North Twenty'):
-		continue
-	print('    "{}": '.format(dStates[s])+"{", file=f)
-	print('{:8}"Name": "{}",'.format(" ",s), file=f)
-	i = 0
-	tDistricts = p[p.State == s].District.unique().astype(str)
-	print(s, tDistricts)
-	tDistricts.sort()
-	print(tDistricts.shape[0])
-	for d in tDistricts:
-		i += 1
-		if (i >= tDistricts.shape[0]):
-			tTerm = ""
-		else:
-			tTerm = ","
-		print('{:8}"DId{:02}": "{}"{}'.format(" ",i,d,tTerm), file=f)
-	print("{:8}".format(' ')+"},", file=f)
-print('}', file=f)
-f.close()
+
+def gen_regions(fName, dStates):
+    p=pandas.read_csv(fName)
+    f = open("/tmp/regions.json","w+")
+    print('{', file=f)
+    for s in p.State.unique():
+        if s.startswith('North Twenty'):
+            continue
+        print('    "{}": '.format(dStates[s])+"{", file=f)
+        print('{:8}"Name": "{}",'.format(" ",s), file=f)
+        i = 0
+        tDistricts = p[p.State == s].District.unique().astype(str)
+        print(s, tDistricts)
+        tDistricts.sort()
+        print(tDistricts.shape[0])
+        for d in tDistricts:
+            i += 1
+            if (i >= tDistricts.shape[0]):
+                tTerm = ""
+            else:
+                tTerm = ","
+            print('{:8}"DId{:02}": "{}"{}'.format(" ",i,d,tTerm), file=f)
+        print("{:8}".format(' ')+"},", file=f)
+    print('}', file=f)
+    f.close()
+
+
+dStates = get_statecodes("./statesuts.code.csv")
+gen_regions("hospital_directory.csv", dStates)
+
+
+
+# vim: set sts=4 ts=4 sw=4 expandtab: #
