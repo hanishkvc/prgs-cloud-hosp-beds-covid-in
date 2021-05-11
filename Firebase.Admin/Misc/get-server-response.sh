d3f64ef0585e5a80deb0f8eee0eed447219d3838
hanishkvc <hanishkvc@gmail.com>
2021-05-11 21:22:39 +0530

    Make server response check generic

--- Firebase.Admin/Misc/get-server-response.sh
+++ Firebase.Admin/Misc/get-server-response.sh
@@ -1,2 +1,2 @@
-wget --server-response hosp-beds-covid-in.web.app 2> server-response.txt
-wget --server-response hosp-beds-covid-in.web.app/scripts/ui.js 2>> server-response.txt
+wget --server-response $FBASE_WEBAPP 2> server-response.txt
+wget --server-response $FBASE_WEBAPP/scripts/ui.js 2>> server-response.txt
